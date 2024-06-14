2010ca02aedcaba7294387b3bca96aeec57e3a63
Emil Fredriksen <emil.fredriksen@hotmail.no>
2024-06-14 17:48:25 +0200

    header before hover implement

---
+++ src/components/Header.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import {
+  HiHome,
+  HiMagnifyingGlass,
+  HiStar,
+  HiPlayCircle,
+  HiTv,
+} from "react-icons/hi2";
+import { HiPlus, HiDotsVertical } from "react-icons/hi";
+import HeaderItem from "./HeaderItem";
+
+function Header() {
+  const menu = [
+    {
+      name: "HOME",
+      icon: HiHome,
+    },
+    {
+      name: "SEARCH",
+      icon: HiMagnifyingGlass,
+    },
+    {
+      name: "WATCH LIST",
+      icon: HiPlus,
+    },
+
+    {
+      name: "MOVIES",
+      icon: HiPlayCircle,
+    },
+    {
+      name: "SERIES",
+      icon: HiTv,
+    },
+    {
+      name: "ORIGINALS",
+      icon: HiStar,
+    },
+  ];
+
+  return (
+    <div className="flex items-center justify-between p-5">
+      <div className="flex gap-8 items-center">
+        <div className="w-[80px] md:w-[115px] object-cover">Disnot-</div>
+        <div className=" hidden md:flex gap-8">
+          {menu.map((item) => (
+            <HeaderItem name={item.name} Icon={item.icon} />
+          ))}
+        </div>
+
+        <div className="flex md:hidden gap-5">
+          {menu.map(
+            (item, index) =>
+              index < 3 && <HeaderItem name={""} Icon={item.icon} />
+          )}
+        </div>
+
+        <div className="md:hidden">
+          <HeaderItem name={""} Icon={HiDotsVertical} />
+          <div className="absolute mt-3 bg-[#121212] border-[1px] border-gray-800 rounded-md py-4 pl-5 pr-10">
+            {menu.map(
+              (item, index) =>
+                index > 2 && <HeaderItem name={item.name} Icon={item.icon} />
+            )}
+          </div>
+        </div>
+      </div>
+      <img
+        src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+        alt="user-avatar-icon"
+        className="w-10 rounded-full"
+      />
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/HeaderItem.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface HeaderItemProps {
+  name: string;
+  Icon: React.ComponentType;
+}
+
+function HeaderItem({ name, Icon }: HeaderItemProps) {
+  return (
+    <div className="text-white flex items-center gap-3 text-lg font-semibold cursor-pointer text-nowrap hover:underline underline-offset-8 mb-2">
+      <Icon />
+      <h2 className="">{name}</h2>
+    </div>
+  );
+}
+
+export default HeaderItem;
