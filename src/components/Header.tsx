960caeb5424e38c8c4cd4ead0ac360192ea1cd10
Emil Fredriksen <emil.fredriksen@hotmail.no>
2024-06-14 18:03:22 +0200

    added hover drop down menu to nav header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   HiHome,
   HiMagnifyingGlass,
@@ -10,6 +10,9 @@ import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import HeaderItem from "./HeaderItem";
 
 function Header() {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
   const menu = [
     {
       name: "HOME",
@@ -23,7 +26,6 @@ function Header() {
       name: "WATCH LIST",
       icon: HiPlus,
     },
-
     {
       name: "MOVIES",
       icon: HiPlayCircle,
@@ -38,29 +40,57 @@ function Header() {
     },
   ];
 
+  useEffect(() => {
+    if (isHovered) {
+      setIsVisible(true);
+    } else {
+      const timeout = setTimeout(() => {
+        setIsVisible(false);
+      }, 500); // Match this duration with the CSS transition duration
+
+      return () => clearTimeout(timeout);
+    }
+  }, [isHovered]);
+
   return (
     <div className="flex items-center justify-between p-5">
       <div className="flex gap-8 items-center">
         <div className="w-[80px] md:w-[115px] object-cover">Disnot-</div>
-        <div className=" hidden md:flex gap-8">
+        <div className="hidden md:flex gap-8">
           {menu.map((item) => (
-            <HeaderItem name={item.name} Icon={item.icon} />
+            <HeaderItem key={item.name} name={item.name} Icon={item.icon} />
           ))}
         </div>
 
         <div className="flex md:hidden gap-5">
           {menu.map(
             (item, index) =>
-              index < 3 && <HeaderItem name={""} Icon={item.icon} />
+              index < 3 && (
+                <HeaderItem key={item.name} name={""} Icon={item.icon} />
+              )
           )}
         </div>
 
-        <div className="md:hidden">
+        <div
+          className="md:hidden relative"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
           <HeaderItem name={""} Icon={HiDotsVertical} />
-          <div className="absolute mt-3 bg-[#121212] border-[1px] border-gray-800 rounded-md py-4 pl-5 pr-10">
+          <div
+            className={`absolute mt-3 bg-[#121212] border-[1px] border-gray-800 rounded-md py-4 pl-5 pr-10 transition-opacity duration-300 ${
+              isHovered ? "opacity-100" : "opacity-0"
+            } ${isVisible ? "visible" : "invisible"}`}
+          >
             {menu.map(
               (item, index) =>
-                index > 2 && <HeaderItem name={item.name} Icon={item.icon} />
+                index > 2 && (
+                  <HeaderItem
+                    key={item.name}
+                    name={item.name}
+                    Icon={item.icon}
+                  />
+                )
             )}
           </div>
         </div>
